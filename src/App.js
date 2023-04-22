5838d865f45c8c38435b0fc5600c9de0e8fe06cf
Kdangelo <kdangelof@gmail.com>
2023-04-22 18:35:32 -0400

    Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Cards from './components/Cards';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
         descript={"Mujer de pelo rojo y pecas"} />
       <Cards img={"https://cdn.pixabay.com/photo/2018/01/13/19/39/fashion-3080644_1280.jpg"} title={"Mujer morena"} 
         descript={"Mujer de pelo y piel marron"} />
-      <Cards img={"https://cdn.pixabay.com/photo/2018/01/06/09/25/hijab-3064633_1280.jpg"} title={"Mujer arabe"} 
+      <Cards img={"https://cdn.pixabay.com/photo/2018/01/06/09/25/hijab-3064633_1280.jpg"} title={"Mujer musulmana"} 
         descript={"Mujer de Hijab y religiosa"} />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <div>
+      <hr/>
+      <h3>Todos los derechos reservados</h3>
+    </div>
+  );
+}
+
+export default Footer;
